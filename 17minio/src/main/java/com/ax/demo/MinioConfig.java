bdc2ab83081fcff36451742e19ed42840618f0a6
axinger <axingrun@outlook.com>
2020-12-14 16:05:08 +0800

    update…

---
+++ 17minio/src/main/java/com/ax/demo/MinioConfig.java
@@ -0,0 +1,41 @@
+package com.ax.demo;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(MinioProp.class)
+public class MinioConfig {
+
+    @Autowired
+    private MinioProp minioProp;
+
+    /**package com.minio.dto.response;
+
+     import lombok.AllArgsConstructor;
+     import lombok.Data;
+     import lombok.NoArgsConstructor;
+
+     @Data
+     @AllArgsConstructor
+     @NoArgsConstructor
+     public class FileUploadResponse {
+     private String urlHttp;
+
+     private String urlPath;
+     }
+
+      * 获取MinioClient
+     */
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                        .endpoint(minioProp.getEndpoint())
+                        .credentials(minioProp.getAccessKey(), minioProp.getSecretKey())
+                        .build();
+    }
+
+}
