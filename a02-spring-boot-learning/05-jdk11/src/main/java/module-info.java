725746f66bbb79182a4b65624b691f6d48133ba1
axinger <liu_weixing@qq.com>
2022-07-27 21:26:07 +0800

    update…

--- a02-spring-boot-learning/05-jdk11/src/main/java/module-info.java
+++ a02-spring-boot-learning/05-jdk11/src/main/java/module-info.java
@@ -2,4 +2,6 @@ module java9Demo {
 
     requires spring.boot.autoconfigure;
     requires spring.boot;
+    requires spring.web;
+    requires java.net.http;
 }
---
+++ a02-spring-boot-learning/05-test-jdk11/src/main/java/com/axing/controller/IndexController.java
@@ -0,0 +1,67 @@
+package com.axing.controller;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xing
+ * @version 1.0.0
+ * @ClassName IndexController.java
+ * @description TODO
+ * @createTime 2022年07月27日 21:02:00
+ */
+
+@RestController
+public class IndexController {
+
+    @PostMapping("/data1")
+    public Object data1(@RequestBody Map map) {
+        Map resultMap = new HashMap();
+        resultMap.put("date", new Date());
+        resultMap.putAll(map);
+        return resultMap;
+    }
+
+
+    @GetMapping("/post")
+    void test1() throws IOException, InterruptedException {
+        Map map = new HashMap();
+        map.put("name", "jim");
+
+        String data = JSON.toJSONString(map);// json 请求数据
+//        HttpRequest request = HttpRequest.newBuilder(URI.create("http://127.0.0.1:8080/data1"))
+//                .POST(HttpRequest.BodyPublishers.ofString(data))
+//                .header("Content-Type", "application/json") //设置头部信息
+//                .timeout(Duration.ofSeconds(10)) // 设置响应超时时间
+//                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(data))
+                .header("Content-Type", "application/json") //设置头部信息
+                .uri(URI.create("http://127.0.0.1:8080/data1"))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+        System.out.println(response.body());
+        final JSONObject jsonObject = JSON.parseObject(response.body());
+
+
+        System.out.println("JSON.parseObject(response.body()) = " + jsonObject + " = " + jsonObject.get("name"));
+    }
+}
