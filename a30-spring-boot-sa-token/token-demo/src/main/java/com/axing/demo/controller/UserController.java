264862ddf55c846c9b847eb3a844730dd86cfba6
axingRun <liu_weixing@qq.com>
2022-11-25 17:43:36 +0800

    sa-token

---
+++ a30-spring-boot-sa-token/token-demo/src/main/java/com/axing/demo/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.axing.demo.controller;
+
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
+import com.axing.common.response.result.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+
+    // 登录接口
+    @PostMapping("/doLogin")
+    public Result doLogin(String username, String password) {
+        // 第1步，先登录上
+        //StpUtil.login(10001);
+        // 设置登录账号id为10001，第二个参数指定是否为[记住我]，当此值为false后，关闭浏览器后再次打开需要重新登录
+        StpUtil.login(10001, false);
+        // 第2步，获取 Token  相关参数
+        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+        // 第3步，返回给前端
+        return Result.ok(tokenInfo);
+    }
+
+    // 查询登录状态  ---- http://localhost:8081/acc/isLogin
+    @GetMapping("isLogin")
+    public Result isLogin() {
+        return Result.ok("是否登录：" + StpUtil.isLogin());
+    }
+
+    // 查询 Token 信息  ---- http://localhost:8081/acc/tokenInfo
+    @GetMapping("tokenInfo")
+    public Result tokenInfo() {
+        return Result.ok(StpUtil.getTokenInfo());
+    }
+
+    // 测试注销  ---- http://localhost:8081/acc/logout
+    @GetMapping("logout")
+    public Result logout() {
+        StpUtil.logout();
+        return Result.ok();
+    }
+
+}
