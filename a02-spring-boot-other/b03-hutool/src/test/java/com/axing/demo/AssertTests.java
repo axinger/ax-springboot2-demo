2c577a47d6bffc1f78081f665be5cd393f8f89a6
axingRun <liu_weixing@qq.com>
2022-12-09 13:03:35 +0800

    update

--- a02-spring-boot-other/b03-hutool/src/test/java/com/axing/demo/AssertTests.java
+++ a02-spring-boot-other/b03-hutool/src/test/java/com/axing/demo/AssertTests.java
@@ -10,8 +10,9 @@ public class AssertTests {
     void test() {
 
         String a = null;
+        // String a = "1";
         // 表达式如果是true程序继续执行，如果是false就会报错告诉你这里错了
-        Assert.notBlank(a, "a不能为空");
+        Assert.notBlank(a, "a不能为空{}{}",1,2);
         System.out.println("222222222222222");
     }
 }
