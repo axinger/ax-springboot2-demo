cc10f5462fe060a198f149200cb41636892418d9
xingRun <liu_weixing@qq.com>
2022-12-17 10:12:50 +0800

    update

---
+++ a09-spring-boot-es2/src/main/java/demo/model/Book.java
@@ -0,0 +1,10 @@
+package demo.model;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+}
--- a09-spring-boot-es2/src/test/java/demo/ESDemoApplicationTest.java
+++ a09-spring-boot-es2/src/test/java/demo/ESDemoApplicationTest.java
@@ -1,17 +1,42 @@
 package demo;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import demo.model.Book;
 import lombok.SneakyThrows;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class ESDemoApplicationTest {
 
     @Autowired
     private ElasticsearchClient client;
+
+    @Test
+    public void test07() throws IOException {
+        Book book = new Book();
+        book.setId(890);
+        book.setName("深入理解Java虚拟机");
+        book.setAuthor("xxx");
+        //添加一个文档
+        //这是一个同步请求，请求会卡在这里
+        IndexResponse response = client.index(i -> i.index("books").document(book).id("890"));
+        System.out.println("response.result() = " + response.result());
+        System.out.println("response.id() = " + response.id());
+        System.out.println("response.seqNo() = " + response.seqNo());
+        System.out.println("response.index() = " + response.index());
+        System.out.println("response.shards() = " + response.shards());
+    }
 
     @SneakyThrows
     @Test
