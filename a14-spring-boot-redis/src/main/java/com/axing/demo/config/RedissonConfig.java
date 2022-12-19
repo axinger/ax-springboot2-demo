f198a259267788b7c3e6d4c2dbdc3fab9ca939fa
xingRun <liu_weixing@qq.com>
2022-12-19 14:12:16 +0800

    update

---
+++ a14-spring-boot-redis/src/main/java/com/axing/demo/config/RedissonConfig.java
@@ -0,0 +1,11 @@
+package com.axing.demo.config;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+}
--- a14-spring-boot-redis/src/main/java/com/axing/demo/service/SecondsKillService.java
+++ a14-spring-boot-redis/src/main/java/com/axing/demo/service/SecondsKillService.java
@@ -1,5 +1,7 @@
 package com.axing.demo.service;
 
+import java.util.Map;
+
 public interface SecondsKillService {
 
     boolean doSecKill(Integer userId, Integer prodId);
@@ -15,4 +17,12 @@ public interface SecondsKillService {
     boolean doSecKillOfRedissonLock(Integer userId, Integer prodId);
 
 
+    /**
+     * 添加秒杀产品库存
+     * @param prodId
+     * @return
+     */
+    Integer addSk(Integer prodId);
+
+    Map<String,Object> killExecute();
 }
