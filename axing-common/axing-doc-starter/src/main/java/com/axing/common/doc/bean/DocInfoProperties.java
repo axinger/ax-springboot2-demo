d12ba7b1bb28606ebb1b9735b95ac76f7cd78832
xingRun <liu_weixing@qq.com>
2022-12-21 13:48:06 +0800

    update

--- axing-common/axing-doc-starter/src/main/java/com/axing/common/doc/bean/DocInfoProperties.java
+++ axing-common/axing-doc-starter/src/main/java/com/axing/common/doc/bean/DocInfoProperties.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties(prefix = "axing.doc-info")
+@ConfigurationProperties(prefix = "axing.doc")
 public class DocInfoProperties {
 
     /**
