621974b6b72372c2156671a8167a667feb4d67d8
axinger <liu_weixing@qq.com>
2023-01-08 19:34:54 +0800

    update…

--- a02-spring-boot-other/b18-spring-aop/src/main/java/com/axing/demo/AopDemoMain.java
+++ a02-spring-boot-other/b18-spring-aop/src/main/java/com/axing/demo/AopDemoMain.java
@@ -1,14 +1,12 @@
 package com.axing.demo;
 
-import com.sun.tools.javac.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class AopDemoMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(AopDemoMain.class, args);
     }
 }
