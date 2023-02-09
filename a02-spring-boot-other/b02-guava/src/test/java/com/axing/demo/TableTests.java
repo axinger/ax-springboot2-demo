8374cf034a133a772589efe3544dbeb8f0a7a4ca
xingRun <liu_weixing@qq.com>
2023-02-09 14:16:03 +0800

    update

--- a02-spring-boot-other/b02-guava/src/test/java/com/axing/demo/TableTests.java
+++ a02-spring-boot-other/b02-guava/src/test/java/com/axing/demo/TableTests.java
@@ -1,7 +1,6 @@
 package com.axing.demo;
 
 import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Table;
 import org.junit.jupiter.api.Test;
 
@@ -31,14 +30,14 @@ public class TableTests {
     @Test
     void test2() {
 
-        List<String> name = List.of("小明","小红");
-        List<String> sub = List.of("数学","语文","英语");
+        List<String> name = List.of("小明", "小红");
+        List<String> sub = List.of("数学", "语文", "英语");
 
-        Table<String, String, Double> tables =  HashBasedTable.create();
+        Table<String, String, Double> tables = HashBasedTable.create();
         // 补齐,矩阵空值
         for (String s : name) {
             for (String s1 : sub) {
-                tables.put(s,s1,0.0); // 默认0
+                tables.put(s, s1, 0.0); // 默认0
             }
         }
         System.out.println("tables.rowKeySet() = " + tables.rowKeySet());
@@ -59,7 +58,7 @@ public class TableTests {
         System.out.println("tables.columnMap() = " + tables.columnMap());
 
 
-        tables.cellSet().stream().forEach((k1) -> {
+        tables.cellSet().forEach((k1) -> {
             System.out.println("k1.getRowKey() = " + k1.getRowKey());
             System.out.println("k1.getColumnKey() = " + k1.getColumnKey());
             System.out.println("k1.getValue() = " + k1.getValue());
