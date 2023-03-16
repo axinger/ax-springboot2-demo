58810fcbb0928317887072858b248b0f242dc3bf
axingRun <liu_weixing@qq.com>
2023-03-16 10:04:48 +0800

    update

--- a02-spring-boot-other/b06-future-async/src/test/java/com/axing/future/Demo6ApplicationTest.java
+++ a02-spring-boot-other/b06-future-async/src/test/java/com/axing/future/Demo6ApplicationTest.java
@@ -1,5 +1,8 @@
 package com.axing.future;
 
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.axing.future.model.PersonEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -9,9 +12,11 @@ import org.springframework.util.StopWatch;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 @SpringBootTest
 @Slf4j
@@ -157,5 +162,77 @@ class Demo6ApplicationTest {
         log.info("结果: 耗时 = {},线程 = {},entity = {},entity1 = {}", stopWatch.getTotalTimeSeconds(), Thread.currentThread().getName(), entity, entity1);
 
 
+    }
+
+    @Test
+    void test3() {
+        List<CompletableFuture> futures = new ArrayList<>(3);
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("A==========================超时了,最后还会走,但不执行返回数据");
+                    return "A";
+                }, executor)
+                .orTimeout(2, TimeUnit.SECONDS)
+                .exceptionally((e) -> {
+                    System.out.println("e = " + e);
+                    return "-A";
+                });
+        futures.add(future);
+
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            log.info("B===================");
+            return "B";
+        }, executor);
+        futures.add(future1);
+
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            log.info("C===================");
+            if (true) {
+                throw new RuntimeException("-C");
+            }
+            return "-C";
+        }, executor).exceptionally((e) -> {
+            System.out.println("e = " + e);
+            return "-C";
+        });
+
+        futures.add(future2);
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .exceptionally(e -> {
+                    System.out.println("allOf e = " + e);
+                    return null;
+                }).join();
+        stopWatch.stop();
+        System.out.println("future.join() = " + future.join());
+        System.out.println("future1.join() = " + future1.join());
+        System.out.println("future2.join() = " + future2.join());
+        System.out.println("stopWatch.getTotalTimeSeconds() = " + stopWatch.getTotalTimeSeconds());
+
+        System.out.println("结束=========================================");
+        try {
+            TimeUnit.SECONDS.sleep(60);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
