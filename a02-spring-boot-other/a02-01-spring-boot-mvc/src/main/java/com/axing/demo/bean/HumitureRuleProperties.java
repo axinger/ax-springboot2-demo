9db48c628744a82b691560c85f76bb8b88b6f569
axinger <liu_weixing@qq.com>
2023-03-23 16:04:20 +0800

    update

---
+++ a02-spring-boot-other/a02-01-spring-boot-mvc/src/main/java/com/axing/demo/bean/HumitureRuleProperties.java
@@ -0,0 +1,13 @@
+package com.axing.demo.bean;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+// @Configuration // record 不能使用
+@ConfigurationProperties(prefix = "show")
+public record HumitureRuleProperties(HumidityDto humidity, HumidityDto temperature) {
+
+    public record HumidityDto(double min, double max) {
+    }
+
+}
---
+++ a02-spring-boot-other/a02-03-hutool/src/test/java/com/axing/demo/DateTests.java
@@ -0,0 +1,18 @@
+package com.axing.demo;
+
+import cn.hutool.core.date.DateUtil;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class DateTests {
+
+    @Test
+    void test1() {
+        // LocalDateTime 转 DateTime extends Date
+        Date date = DateUtil.date(LocalDateTime.now());
+        System.out.println("date = " + date);
+        System.out.println("new Date() = " + new Date());
+    }
+}
