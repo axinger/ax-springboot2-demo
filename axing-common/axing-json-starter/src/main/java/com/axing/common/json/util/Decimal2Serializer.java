17b7e3d67e17c0caf37c8c6b7f04c96858a16b48
axinger <liu_weixing@qq.com>
2023-04-21 17:01:26 +0800

    update

---
+++ axing-common/axing-json-starter/src/main/java/com/axing/common/json/util/Decimal2Serializer.java
@@ -0,0 +1,32 @@
+package com.axing.common.json.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * 使用@JsonSerialize(using = Decimal0Serializer.class)
+ */
+public class Decimal2Serializer extends JsonSerializer<Object> {
+
+    /**
+     * 将返回的BigDecimal保留两位小数，再返回给前端
+     *
+     * @param value
+     * @param jsonGenerator
+     * @param serializerProvider
+     * @throws IOException
+     */
+    @Override
+    public void serialize(Object value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        if (value != null) {
+            BigDecimal bigDecimal = new BigDecimal(String.valueOf(value)).setScale(2, RoundingMode.HALF_UP);
+            jsonGenerator.writeString(bigDecimal.toString());
+        }
+    }
+}
