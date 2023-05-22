d25581a94b42fe5c304eaef99fbf6d84b2275f2f
axinger <liu_weixing@qq.com>
2023-05-22 16:25:44 +0800

    update

--- a31-poi/src/test/java/com/axing/demo/PoiTests.java
+++ a31-poi/src/test/java/com/axing/demo/PoiTests.java
@@ -1,7 +1,12 @@
 package com.axing.demo;
 
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import lombok.SneakyThrows;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.usermodel.TableCell;
+import org.apache.poi.hwpf.usermodel.TableIterator;
+import org.apache.poi.hwpf.usermodel.TableRow;
 import org.apache.poi.xwpf.usermodel.IBodyElement;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
@@ -9,16 +14,16 @@ import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Objects;
 
 public class PoiTests {
 
     @SneakyThrows
     @Test
     public void test1() {
-
-
         InputStream inputStream = new FileInputStream("D:\\Users\\cepai\\Desktop\\123.docx");
         XWPFDocument document = new XWPFDocument(inputStream);
         // 获取word中的所有段落与表格
@@ -35,5 +40,30 @@ public class PoiTests {
                 System.out.println("表格text = " + table);
             }
         }
+    }
+
+    /**
+     * 读取doc
+     * @throws IOException
+     */
+    @Test
+    void test2() throws IOException {
+        com.google.common.collect.Table<Integer, Integer,String> tables = HashBasedTable.create();
+        HWPFDocument doc = new HWPFDocument(new FileInputStream("D:\\Users\\cepai\\Desktop\\123.doc"));
+        TableIterator tableIter = new TableIterator(doc.getRange());
+        while (tableIter.hasNext()) {
+            org.apache.poi.hwpf.usermodel.Table table = tableIter.next();
+            for (int i = 0; i < table.numRows(); i++) {
+                TableRow row = table.getRow(i);
+                for (int j = 0; j < row.numCells(); j++) {
+                    TableCell cell = row.getCell(j);
+                    String content = cell.getParagraph(0).text();
+                    tables.put(i,j,content);
+                }
+            }
+        }
+        doc.close();
+        System.out.println("tables = " + tables);
+        System.out.println("tables.get(1,1) = " + Objects.requireNonNull(tables.get(1, 1)).replace("\u0007",""));
     }
 }
