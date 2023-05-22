9d56ea618bbe4b53dae5bf0f4388856555e180ab
axinger <liu_weixing@qq.com>
2023-05-22 14:48:37 +0800

    update

---
+++ a35-flowable/src/main/java/com/axing/demo/handler/BossTaskHandler.java
@@ -0,0 +1,11 @@
+package com.axing.demo.handler;
+
+import org.flowable.engine.delegate.TaskListener;
+import org.flowable.task.service.delegate.DelegateTask;
+
+public class BossTaskHandler implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        delegateTask.setAssignee("老板");
+    }
+}
