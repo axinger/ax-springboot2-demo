bc3eaf45a73396bcddfd9bba769b8b35f0864616
axinger <liu_weixing@qq.com>
2023-07-27 15:59:10 +0800

    update

---
+++ axing-common/axing-http-starter/src/main/java/com/axing/common/http/config/RestTemplateConfig.java
@@ -0,0 +1,25 @@
+package com.axing.common.http.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        // ms
+        factory.setReadTimeout(30 * 1000);
+        // ms
+        factory.setConnectTimeout(30 * 1000);
+        return factory;
+    }
+}
