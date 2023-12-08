d8b641386ad10a0a72fefd7f3270cd3b31e6eec3
axinger <liu_weixing@qq.com>
2023-12-08 11:17:03 +0800

    update

--- a04-spring-boot-db/a04-spring-boot-mybatis-flex/src/main/java/com/github/axinger/domain/Account.java
+++ a04-spring-boot-db/a04-spring-boot-mybatis-flex/src/main/java/com/github/axinger/domain/Account.java
@@ -10,7 +10,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.Comment;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Transient;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -24,37 +29,55 @@ import java.time.LocalDateTime;
 @Table(value = "tb_account")
 @Accessors(chain = true)
 @Data
+@Entity(name = "tb_account")
 //@AllArgsConstructor
 //@NoArgsConstructor
 //@Data(staticConstructor = "create") // 会私有无参构造方法
 public class Account extends Model<Account>  implements Serializable {
 
     @Id(keyType = KeyType.Auto)
+    @javax.persistence.Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @javax.persistence.Column(name = "id")
+    @Comment("id")
     private Integer id;
 
     @Column(value = "user_name")
+    @javax.persistence.Column(name = "user_name")
+    @Comment("用户名")
     private String userName;
 
     @Column(value = "age")
+    @javax.persistence.Column(name = "age")
+    @Comment("年龄")
     private Integer age;
 
     @Column(value = "birthday")
+    @javax.persistence.Column(name = "birthday")
+    @Comment("生日")
     private LocalDateTime birthday;
 
     //最大年龄
     @Column(ignore = true)
+    @Transient
     private int maxAge;
+
     @Column(ignore = true)
+    @Transient
     private int mimAge;
 
     //平均年龄
     @Column(ignore = true)
+    @Transient
     private int avgAge;
 
     // 分组后多个id逗号分割
     @Column(ignore = true)
+    @Transient
     private String groupAfterId;
+
     @Column(ignore = true)
+    @Transient
     private String groupAfterName;
 
 
