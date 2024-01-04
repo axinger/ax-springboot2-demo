0d8af67f01d7f28893edac9ed5557923a1be6004
axinger <liu_weixing@qq.com>
2024-01-04 10:19:27 +0800

    update

--- a02-spring-boot-other/a02-12-spring-web-flux/src/test/java/com/axing/FluxDemoApplicationTests.java
+++ a02-spring-boot-other/a02-12-spring-web-flux/src/test/java/com/axing/FluxDemoApplicationTests.java
@@ -3,11 +3,36 @@ package com.axing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.Flow;
+
 @SpringBootTest
 class FluxDemoApplicationTests {
 
     @Test
     void contextLoads() {
+
+        Flow.Subscriber<String> subscriber = new Flow.Subscriber<String>() {
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+
+            }
+
+            @Override
+            public void onNext(String item) {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
     }
 
 }
