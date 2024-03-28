07805b0e7f19ddd398db7cd6a24da6a59065a28e
axinger <liu_weixing@qq.com>
2024-03-28 20:21:25 +0800

    update

--- a01-spring-boot-other/a01-24-event/src/main/java/com/github/axinger/event/EventProducer.java
+++ a01-spring-boot-other/a01-24-event/src/main/java/com/github/axinger/event/EventProducer.java
@@ -4,26 +4,42 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Component
 public class EventProducer<E extends ApplicationEvent> {
 
-    private final List<ApplicationListener<E>> customEventListeners = new ArrayList<>();
+    // 使用CopyOnWriteArrayList以确保线程安全
+    private final List<ApplicationListener<E>> customEventListeners = new CopyOnWriteArrayList<>();
+
+    private static final Logger LOGGER = Logger.getLogger(EventProducer.class.getName());
 
     public void produceEvent(E o) {
-        // 通知所有注册的监听器
+        // 通知所有注册的监听器，添加异常处理以确保一个监听器的失败不会影响其他监听器
         for (ApplicationListener<E> listener : customEventListeners) {
-            listener.onApplicationEvent(o);
+            try {
+                listener.onApplicationEvent(o);
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, "Error occurred while notifying event listener", e);
+
+            }
         }
     }
 
     public void addCustomEventListener(ApplicationListener<E> listener) {
-        customEventListeners.add(listener);
+        // 添加监听器时的线程安全操作
+        synchronized (customEventListeners) {
+            customEventListeners.add(listener);
+        }
     }
 
     public void removeCustomEventListener(ApplicationListener<E> listener) {
-        customEventListeners.remove(listener);
+        // 删除监听器时的线程安全操作
+        synchronized (customEventListeners) {
+            customEventListeners.remove(listener);
+        }
     }
 }
--- a01-spring-boot-other/a01-24-event/src/main/java/com/github/axinger/service/IMyServiceImp.java
+++ a01-spring-boot-other/a01-24-event/src/main/java/com/github/axinger/service/IMyServiceImp.java
@@ -2,36 +2,40 @@ package com.github.axinger.service;
 
 import com.github.axinger.event.CustomEvent;
 import com.github.axinger.event.EventProducer;
+import lombok.NonNull;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class IMyServiceImp implements IMyService {
-
     private final CustomEventListener customEventListener = new CustomEventListener();
-    int count;
+    // 使用AtomicInteger保证线程安全
+    private final AtomicInteger count = new AtomicInteger();
+    private volatile boolean isListening = false;
+
     @Autowired
     private EventProducer<CustomEvent> eventProducer;
-    private boolean isListening = false;
 
     @SneakyThrows
     @Override
     public void test(String name) {
-        // 开始监听事件
-        startListening();
+        count.set(0);
+        try {
+            startListening();
 
-        // 执行一些代码
-        while (count < 3) {
-            System.out.println("name = " + name + "count = " + count);
-            TimeUnit.SECONDS.sleep(2);
+            while (count.get() < 3) {
+                System.out.println("name = " + name + "count = " + count.get());
+                TimeUnit.SECONDS.sleep(2);
+            }
+        } finally {
+            // 确保在方法结束时调用stopListening，避免资源泄露
+            stopListening();
         }
-
-        // 结束监听事件
-        stopListening();
     }
 
     private void startListening() {
@@ -53,9 +57,9 @@ public class IMyServiceImp implements IMyService {
     // 内部类，用于处理自定义事件
     private class CustomEventListener implements ApplicationListener<CustomEvent> {
         @Override
-        public void onApplicationEvent(CustomEvent event) {
-            System.out.println("event = " + event);
-            count = (Integer) event.getSource();
+        public void onApplicationEvent(@NonNull CustomEvent event) {
+            // 使用getAndIncrement保证线程安全的递增
+            count.set((Integer) event.getSource());
         }
     }
 }
