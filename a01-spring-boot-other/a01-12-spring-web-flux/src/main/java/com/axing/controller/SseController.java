03dd87de6ae91dcae40b85fb1512469c8567c4a0
axinger <liu_weixing@qq.com>
2024-05-17 10:38:41 +0800

    update

--- a01-spring-boot-other/a01-12-spring-web-flux/src/main/java/com/axing/controller/SseController.java
+++ a01-spring-boot-other/a01-12-spring-web-flux/src/main/java/com/axing/controller/SseController.java
@@ -1,10 +1,12 @@
 package com.axing.controller;
 
+import com.axing.service.EventService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,10 +17,9 @@ import reactor.core.publisher.Sinks;
 @RestController
 public class SseController {
 
-    @RequestMapping(value = "/sse.page")
     @Operation(summary = "sse页面 ", description = "description注解")
+    @RequestMapping(value = "/sse.page")
     public Object ipLogPageInfo1() {
-
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("sse");
         return modelAndView;
@@ -27,9 +28,22 @@ public class SseController {
     @Autowired
     private Sinks.Many<String> sseSink;
 
-    @Operation(summary = "链接sse ", description = "description注解")
+    @Operation(summary = "链接sse ")
     @GetMapping(value = "/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> streamEvents() {
         return sseSink.asFlux();
     }
+
+
+    @Autowired
+    private EventService eventService;
+
+    @Operation(summary = "发送sse ", description = "description注解")
+    @PostMapping("/send")
+    public void sendEvent(String data) {
+        // 在这里处理数据
+        // 发布事件
+        eventService.publishEvent(data);
+    }
+
 }
