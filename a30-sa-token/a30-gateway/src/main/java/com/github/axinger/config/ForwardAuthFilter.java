d8351a3c2caeb42a50a3c867e1a89e9c86bf7446
axinger <liu_weixing@qq.com>
2024-05-18 22:10:04 +0800

    update

--- a30-sa-token/a30-gateway/src/main/java/com/github/axinger/config/ForwardAuthFilter.java
+++ a30-sa-token/a30-gateway/src/main/java/com/github/axinger/config/ForwardAuthFilter.java
@@ -2,6 +2,7 @@ package com.github.axinger.config;
 
 import cn.dev33.satoken.exception.NotPermissionException;
 import cn.dev33.satoken.stp.StpUtil;
+import lombok.NonNull;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -9,22 +10,12 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Component
 public class ForwardAuthFilter implements WebFilter {
-    static Set<String> whitePaths = new HashSet<>();
 
-
-    static {
-        whitePaths.add("/account/user/doLogin");
-        whitePaths.add("/account/user/logout");
-        whitePaths.add("/account/user/register");
-    }
 
     @Override
-    public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
+    public @NonNull Mono<Void> filter(ServerWebExchange serverWebExchange, @NonNull WebFilterChain webFilterChain) {
 
         ServerHttpRequest serverHttpRequest = serverWebExchange.getRequest();
 
@@ -32,7 +23,7 @@ public class ForwardAuthFilter implements WebFilter {
         String path = serverHttpRequest.getPath().toString();
 
         // 需要校验权限
-        if (!whitePaths.contains(path)) {
+        if (!WhitePath.whitePaths.contains(path)) {
 
             // 判断用户是否有该权限
             if (!StpUtil.hasPermission(path)) {
