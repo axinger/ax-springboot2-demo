df18a9946b9af16fd234612affb5e2267cde9b9f
axinger <liu_weixing@qq.com>
2024-05-31 10:24:13 +0800

    update

--- a35-flowable/src/main/java/com/github/axinger/config/DatasourceConfigurator.java
+++ a35-flowable/src/main/java/com/github/axinger/config/DatasourceConfigurator.java
@@ -1,6 +1,7 @@
 package com.github.axinger.config;
 
 import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.flowable.common.engine.impl.AbstractEngineConfiguration;
 import org.flowable.common.engine.impl.EngineConfigurator;
@@ -10,7 +11,6 @@ import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import javax.sql.DataSource;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-// 必须配置这个文件 org.flowable.common.engine.impl.EngineConfigurator
 @Slf4j
 @Configuration
 public class DatasourceConfigurator implements EngineConfigurator {
@@ -18,26 +18,16 @@ public class DatasourceConfigurator implements EngineConfigurator {
 
     private static final AtomicBoolean initialized = new AtomicBoolean();
 
-//    @Override
-//    public void beforeInit(AbstractEngineConfiguration engineConfiguration) {
-//
-//
-//        if (initialized.compareAndSet(false, true)) {
-//            log.info("启动中动态切换数据源workflow");
-//            DynamicDataSourceContextHolder.push("db_flowable");
-//        }
-//    }
 
     @Override
     public void beforeInit(AbstractEngineConfiguration abstractEngineConfiguration) {
         if (initialized.compareAndSet(false, true)) {
             DataSource dataSource = abstractEngineConfiguration.getDataSource();
-            if (dataSource instanceof TransactionAwareDataSourceProxy) {
-                dataSource = ((TransactionAwareDataSourceProxy) dataSource).getTargetDataSource();
+            if (dataSource instanceof TransactionAwareDataSourceProxy proxy) {
+                dataSource = proxy.getTargetDataSource();
             }
-            if (dataSource instanceof DynamicRoutingDataSource) {
-                DataSource ds = ((DynamicRoutingDataSource) dataSource).getDataSource("db_flowable");
-                abstractEngineConfiguration.setDataSource(ds);
+            if (dataSource instanceof DynamicRoutingDataSource dynamicRoutingDataSource) {
+                abstractEngineConfiguration.setDataSource(dynamicRoutingDataSource.getDataSource("db_flowable"));
             }
 
         }
