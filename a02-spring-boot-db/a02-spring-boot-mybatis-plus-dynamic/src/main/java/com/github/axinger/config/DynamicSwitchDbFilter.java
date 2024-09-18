e39fc3201fb7c2a3ac9e2230b710f65adf44b035
axinger <liu_weixing@qq.com>
2024-09-18 17:11:50 +0800

    update

---
+++ a02-spring-boot-db/a02-spring-boot-mybatis-plus-dynamic/src/main/java/com/github/axinger/config/DynamicSwitchDbFilter.java
@@ -0,0 +1,51 @@
+package com.github.axinger.config;
+
+import cn.hutool.core.util.ObjUtil;
+import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+
+@Order(-10000)
+@Slf4j
+@Component
+// 多租户实现，动态切换数据源，使用nacos配置
+public class DynamicSwitchDbFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        //在DispatcherServlet之前执行
+        HttpServletRequest req = (HttpServletRequest) request;
+        //业务方决定对应的库
+        String orgCode = req.getHeader("org_code");
+        log.info(req.getRequestURI());
+//        if (ObjUtil.isEmpty(orgCode)) {
+//            log.error("header org code is null,can't find db");
+//            throw new RemoteException("header org code is null,can't find db");
+//        }
+//
+//        log.info("switch db name:{}", orgCode);
+//        //切换数据源，就是datasource下面的key
+//        DynamicDataSourceContextHolder.push(orgCode);
+//        filterChain.doFilter(request, response);
+//
+//        //通过ThreadLocal传递库信息，所以需要清楚ThreadLocal
+//        log.info("clean db name:{}", orgCode);
+//        DynamicDataSourceContextHolder.clear();
+    }
+
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ a02-spring-boot-db/a02-spring-boot-mybatis-plus-dynamic/src/test/java/com/github/axinger/Demo.java
@@ -0,0 +1,23 @@
+package com.github.axinger;
+
+import com.baomidou.dynamic.datasource.toolkit.CryptoUtils;
+
+public class Demo {
+
+//    public static void main(String[] args) throws Exception {
+//        String password = "123456";
+//        //使用默认的publicKey ，建议还是使用下面的自定义
+//        String encodePassword = CryptoUtils.encrypt(password);
+//        System.out.println(encodePassword);
+//    }
+
+        //自定义publicKey
+    public static void main(String[] args) throws Exception {
+        String[] arr = CryptoUtils.genKeyPair(512);
+        System.out.println("privateKey:  " + arr[0]);
+        System.out.println("publicKey:  " + arr[1]);
+        System.out.println("url:  " + CryptoUtils.encrypt(arr[0], "jdbc:mysql://127.0.0.1:3306/order"));
+        System.out.println("username:  " + CryptoUtils.encrypt(arr[0], "root"));
+        System.out.println("password:  " + CryptoUtils.encrypt(arr[0], "123456"));
+    }
+}
