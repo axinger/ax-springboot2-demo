fb0a0fa53eced91927c4830d8aa068dd3a1f86f1
axinger <liu_weixing@qq.com>
2024-10-30 20:09:41 +0800

    update

---
+++ a40-ai/src/main/java/com/github/axinger/controlle/AIController.java
@@ -0,0 +1,32 @@
+package com.github.axinger.controlle;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@RestController
+public class AIController {
+    @Resource
+    private OllamaChatClient ollamaChatClient;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(name = "message") String message) {
+        return ollamaChatClient.call(message);
+    }
+
+    @GetMapping(value = "/chat2")
+    public Flux<String> chat2(@RequestParam(name = "message") String message) {
+        Flux<String> flux = ollamaChatClient.stream(message);
+
+
+        flux.doOnNext(System.out::println)
+                .subscribeOn(Schedulers.boundedElastic()) // 使用不同调度器
+                .subscribe();
+        return flux;
+    }
+
+}
