795e111976067396ae6a8913544277f43c15fd78
axinger <liu_weixing@qq.com>
2024-10-30 16:58:46 +0800

    update

--- a38-xxl-job/src/main/java/com/github/axinger/server/SampleXxlJob.java
+++ a38-xxl-job/src/main/java/com/github/axinger/server/SampleXxlJob.java
@@ -1,13 +1,8 @@
 package com.github.axinger.server;
 
-import cn.hutool.core.date.LocalDateTimeUtil;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
-import com.xxl.job.core.util.XxlJobRemotingUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedInputStream;
