0103c49827b95f702e4c40813a73d98204ca08e1
axinger <liu_weixing@qq.com>
2024-11-05 15:30:38 +0800

    update

--- a03-spring-boot-websocket/WebFlux-ws/src/main/java/com/github/axinger/ReactiveWebSocketServerHandler.java
+++ a03-spring-boot-websocket/WebFlux-ws/src/main/java/com/github/axinger/ReactiveWebSocketServerHandler.java
@@ -1,8 +1,8 @@
 package com.github.axinger;
 
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
-import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -10,31 +10,37 @@ public class ReactiveWebSocketServerHandler implements WebSocketHandler {
 
     @Override
     public Mono<Void> handle(WebSocketSession session) {
-        Mono<Void> send = session.send(Flux.create(sink -> {
-            //可以持有sink对象在任意时候调用next发送消息
-//            sink.next( );
-        })).doOnError(it -> {
-            //异常处理
-        });
+//        Mono<Void> send = session.send(Flux.create(sink -> {
+//            //可以持有sink对象在任意时候调用next发送消息
+////            sink.next( );
+//        })).doOnError(it -> {
+//            //异常处理
+//        });
+//
+//        Mono<Void> receive = session.receive()
+//                .doOnNext(it -> {
+//                    //接收消息
+//                })
+//                .doOnError(it -> {
+//                    //异常处理
+//                })
+//                .then();
+//
+//        Disposable disposable = session.closeStatus()
+//                .doOnError(it -> {
+//                    //异常处理
+//                })
+//                .subscribe(it -> {
+//                    //连接关闭
+//                });
+//
+//        return Mono.zip(send, receive).then();
 
-        Mono<Void> receive = session.receive()
-                .doOnNext(it -> {
-                    //接收消息
-                })
-                .doOnError(it -> {
-                    //异常处理
-                })
-                .then();
 
-        @SuppressWarnings("all")
-        Disposable disposable = session.closeStatus()
-                .doOnError(it -> {
-                    //异常处理
-                })
-                .subscribe(it -> {
-                    //连接关闭
-                });
-
-        return Mono.zip(send, receive).then();
+        Flux<WebSocketMessage> messageFlux = session.receive().map(message -> {
+            String payload = message.getPayloadAsText();
+            return "Received: " + payload;
+        }).map(session::textMessage);
+        return session.send(messageFlux);
     }
 }
