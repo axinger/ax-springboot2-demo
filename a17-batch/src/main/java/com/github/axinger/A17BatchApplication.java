db51d58566a6a6ed440c651df58fc53a2a5dea60
axinger <liu_weixing@qq.com>
2024-11-23 03:40:48 +0800

    update

--- a17-batch/src/main/java/com/github/axinger/A17BatchApplication.java
+++ a17-batch/src/main/java/com/github/axinger/A17BatchApplication.java
@@ -1,17 +1,14 @@
 package com.github.axinger;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 
 @SpringBootApplication
 //@SpringBootApplication(exclude = {BatchAutoConfiguratio.class})
 @MapperScan("com.github.axinger.mapper")
-public class Main {
+public class A17BatchApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(A17BatchApplication.class, args);
     }
 }
