fc5e34f61dad018927359172c2212c950476a461
axinger <liu_weixing@qq.com>
2024-12-17 22:16:25 +0800

    update

--- a01-spring-boot-learn/a01-03-hutool/src/test/java/com/github/axinger/CsvTests.java
+++ a01-spring-boot-learn/a01-03-hutool/src/test/java/com/github/axinger/CsvTests.java
@@ -1,6 +1,5 @@
 package com.github.axinger;
 
-import cn.hutool.core.io.file.FileWriter;
 import cn.hutool.core.text.csv.CsvUtil;
 import cn.hutool.core.text.csv.CsvWriteConfig;
 import cn.hutool.core.text.csv.CsvWriter;
@@ -9,21 +8,18 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class CsvTests {
 
     /**
      * 生成User列表
+     *
      * @return List<User>
      */
-    public static List<User> generateUserList(){
+    public static List<User> generateUserList() {
         List<User> dataList = new ArrayList<>();
         dataList.add(new User(1, "张三", "男"));
         dataList.add(new User(2, "李四", "女"));
@@ -34,7 +30,7 @@ public class CsvTests {
         return dataList;
     }
 
-    public CsvWriteConfig config(){
+    public CsvWriteConfig config() {
 
 
         CsvWriteConfig csvWriteConfig = new CsvWriteConfig();
@@ -61,27 +57,15 @@ public class CsvTests {
 
         System.out.println("distFile.getAbsolutePath() = " + distFile.getAbsolutePath());
     }
+
     /**
      * 使用自定CSV配置生成CSV文件
      */
     @Test
-    public  void test1() throws InterruptedException {
-        // 可以通过设置FileWriter的编码来控制输出文件的编码格式
-
-        // 写到临时目录，如果临时目录满了系统会自动清除。
-//        File distFile = new File(System.getProperty("C:\\Users\\xing\\Desktop"), "test.csv");
-
-        File distFile = new File("C:\\Users\\xing\\Desktop\\hutoolCsv.csv");
-
+    public void test1() throws InterruptedException {
+        File distFile = new File("C:\\Users\\xing\\Desktop\\1.csv");
         // 创建CSV写入器，使用GBK编码格式，防止出现各个CSV工具打开乱码
-//        CsvWriter csvWriter = new CsvWriter(distFile, CharsetUtil.CHARSET_GBK,true,config);
-        CsvWriter csvWriter =  CsvUtil.getWriter(distFile, CharsetUtil.CHARSET_UTF_8,true,config());
-//        CsvWriter csvWriter =  CsvUtil.getWriter(distFile);
-
-//        CsvWriter csvWriter =  CsvUtil.getWriter("C:\\Users\\xing\\Desktop\\hutoolCsv.csv" , CharsetUtil.CHARSET_UTF_8,true);
-
-
-
+        CsvWriter csvWriter = CsvUtil.getWriter(distFile, CharsetUtil.CHARSET_GBK, true, config());
         csvWriter.writeHeaderLine("策略id", "账户id");
         List<List<Long>> rowList = new ArrayList<>();
         rowList.add(Arrays.asList(111L, 222L));
@@ -93,18 +77,13 @@ public class CsvTests {
 
     }
 
-
-//
-//    @Test
-//    void testCsv() {
-//
-//        // 构造，覆盖已有文件（如果存在），默认编码UTF-8
-//        File hutoolCsv = new File("hutoolCsv1.csv");
-//        CsvWriter writer = new CsvWriter(hutoolCsv);
-//        // 设置编码
-//        // CsvWriter writer = new CsvWriter(hutoolCsv, StandardCharsets.UTF_8);
-//        writer.writeBeans(users);
-//        writer.flush();
-//        writer.close();
-//    }
+    @Test
+    public void test2() throws InterruptedException {
+        File distFile = new File("C:\\Users\\xing\\Desktop\\2.csv");
+        // 创建CSV写入器，使用GBK编码格式，防止出现各个CSV工具打开乱码
+//        CsvWriter csvWriter = CsvUtil.getWriter(distFile, CharsetUtil.CHARSET_GBK, true, config());
+        CsvWriter csvWriter = CsvUtil.getWriter(distFile, CharsetUtil.CHARSET_GBK, true, config());
+        csvWriter.writeBeans(generateUserList());
+        csvWriter.close();
+    }
 }
