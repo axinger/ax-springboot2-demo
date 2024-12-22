860826d241686cac3942c9f39cb18803a553e076
axinger <liu_weixing@qq.com>
2024-12-22 19:47:45 +0800

    update

---
+++ a01-spring-boot-learn/a01-12-rxjava/src/main/java/com/github/axinger/RxController.java
@@ -0,0 +1,18 @@
+package com.github.axinger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class RxController {
+
+    @GetMapping("/numbers")
+    public Observable<Integer> getNumbers() {
+        return Observable.interval(1, TimeUnit.SECONDS)
+                          .map(tick -> tick.intValue() + 1)
+                          .take(10);
+    }
+}
--- a01-spring-boot-learn/a01-12-rxjava/src/test/java/com/github/axinger/A0112RxjavaApplicationTest.java
+++ a01-spring-boot-learn/a01-12-rxjava/src/test/java/com/github/axinger/A0112RxjavaApplicationTest.java
@@ -1,24 +1,26 @@
 package com.github.axinger;
 
 
-import io.reactivex.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import org.junit.jupiter.api.Test;
 
+import io.reactivex.rxjava3.core.Observable;
 
+import java.util.concurrent.TimeUnit;
 
 public class A0112RxjavaApplicationTest {
 
     @Test
-    void test1() {
+    void test1() throws InterruptedException {
 
-        Observable<String> observable = Observable.create(observer -> {
-            observer.onNext("Hello, world!");
-            observer.onComplete();
-        });
+//        Observable<String> observable = Observable.create(observer -> {
+//            observer.onNext("Hello, world!");
+//            observer.onComplete();
+//        });
 //
 //        observable.subscribe(System.out::println);
 
-        Observable.just("Hello Tester1")
+        Disposable subscribe = Observable.just("Hello Tester1")
                 .doOnNext(s -> System.out.println("doOnNext：" + s))
                 .doFinally(() -> System.out.println("doFinally1"))
                 .doAfterTerminate(() -> System.out.println("doAfterTerminate1"))
@@ -28,6 +30,11 @@ public class A0112RxjavaApplicationTest {
                         s -> System.out.println("onNext：" + s),
                         throwable -> System.out.println("onError" + throwable),
                         () -> System.out.println("onComplete"));
+
+
+        subscribe.dispose();
+
+        TimeUnit.SECONDS.sleep(5);
 
     }
 
