7bafecbb177cd8583537e2210724a018439dd908
axinger <liu_weixing@qq.com>
2025-01-06 14:03:52 +0800

    update

---
+++ a36-logback/src/main/java/com/github/axinger/Start1.java
@@ -0,0 +1,20 @@
+package com.github.axinger;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(value = 1)
+@Slf4j
+public class Start1 implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.error("error = {}", "error 日志");
+        log.info("info = {}", "info 日志");
+        log.debug("debug = {}", "debug 日志");
+        log.warn("info = {}", "warn 日志");
+    }
+}
