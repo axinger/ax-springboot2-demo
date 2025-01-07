438f822bb6f220f3d5eb887b3ab89a26bdc582f6
axinger <liu_weixing@qq.com>
2025-01-07 23:18:37 +0800

    update

--- a02-spring-boot-db/a02-spring-boot-mybatis-plus-dynamic/src/main/java/com/github/axinger/config/DbConfig.java
+++
@@ -1,9 +0,0 @@
-package com.github.axinger.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(value = {"com.github.axinger.mapper"})
-public class DbConfig {
-}
