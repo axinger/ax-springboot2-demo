11830149ecc0e8c0af7b14008332e2acfc5223e5
axinger <liu_weixing@qq.com>
2025-01-21 20:45:57 +0800

    update

---
+++ axinger-common/util-core/src/main/java/com/github/axinger/util/CollectionUtils.java
@@ -0,0 +1,20 @@
+package com.github.axinger.util;
+
+import java.util.List;
+
+public class CollectionUtils {
+
+
+    public static boolean matchesPath(List<String> urls, String path) {
+        return urls.stream().anyMatch(pattern -> matchesPattern(path, pattern));
+    }
+
+    private static boolean matchesPattern(String path, String pattern) {
+        // 将 ** 替换为 .*?，以支持通配符匹配
+        String regexPattern = pattern.replace("/**/", "/.*?");
+        // 在正则表达式的两端加上 ^ 和 $，确保完整匹配路径
+        regexPattern = "^" + regexPattern + "$";
+        return path.matches(regexPattern);
+    }
+
+}
