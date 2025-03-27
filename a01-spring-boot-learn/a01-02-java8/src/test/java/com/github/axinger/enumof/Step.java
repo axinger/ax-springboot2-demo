511b96dd5f407896b751b02d8344a5e81fa3623e
axinger <liu_weixing@qq.com>
2025-03-28 01:18:29 +0800

    update

---
+++ a01-spring-boot-learn/a01-02-java8/src/test/java/com/github/axinger/enumof/Step.java
@@ -0,0 +1,45 @@
+package com.github.axinger.enumof;
+
+public enum Step implements StepItem {
+    STEP1 {
+        @Override
+        public int step(int a, int b) {
+            return a + b;
+        }
+
+        @Override
+        public Step next() {
+            return STEP2;
+        }
+    },
+    STEP2 {
+        @Override
+        public int step(int a, int b) {
+            return a * b;
+        }
+
+        @Override
+        public Step next() {
+            return STEP3;
+        }
+    },
+    STEP3 {
+        @Override
+        public int step(int a, int b) {
+            return a - b;
+        }
+
+        @Override
+        public Step next() {
+            return null; // 或者返回 STEP1 以形成循环
+        }
+    };
+
+    public abstract int step(int a, int b);
+    public abstract Step next();
+}
+
+interface StepItem {
+    int step(int a, int b);
+    Step next();
+}
---
+++ a01-spring-boot-learn/a01-02-java8/src/test/java/com/github/axinger/enumof/StepTests.java
@@ -0,0 +1,13 @@
+package com.github.axinger.enumof;
+
+public class StepTests {
+    public static void main(String[] args) {
+        Step currentStep = Step.STEP1;
+
+        while (currentStep != null) {
+            System.out.println("Current Step: " + currentStep);
+            System.out.println("Result: " + currentStep.step(1, 1));
+            currentStep = currentStep.next();
+        }
+    }
+}
