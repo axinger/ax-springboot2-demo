31fd278aae10fe3ccc93acf725b3f25535449c69
axinger <liu_weixing@qq.com>
2025-03-28 01:38:04 +0800

    update

---
+++ a01-spring-boot-learn/a01-02-java8/src/test/java/com/github/axinger/enumof/Process.java
@@ -0,0 +1,96 @@
+package com.github.axinger.enumof;
+
+public enum Process implements ProcessItem {
+    收货 {
+        @Override
+        public Process next(int a, int b) {
+            // 根据计算结果决定下一个步骤
+            if (a + b > 10) {
+                return 检验;
+            } else {
+                System.out.println("this = " + this);
+
+                previousStep = this;
+                return 打包;
+            }
+        }
+    },
+    检验 {
+        @Override
+        public Process next(int a, int b) {
+            // 根据计算结果决定下一个步骤
+            if (a * b > 20) {
+                return 打包;
+            } else {
+                return 发货;
+            }
+        }
+    },
+    打包 {
+        @Override
+        public Process next(int a, int b) {
+            // 根据计算结果决定下一个步骤
+            if (a - b > 0) {
+                return 发货;
+            } else {
+                return 完成;
+            }
+        }
+    },
+    发货 {
+        @Override
+        public Process next(int a, int b) {
+            // 根据计算结果决定下一个步骤
+            if (a / b > 1) {
+                return 完成;
+            } else {
+                return 打包;
+            }
+        }
+    },
+    完成 {
+        @Override
+        public Process next(int a, int b) {
+            // 完成步骤没有下一个步骤
+            return null;
+        }
+    };
+
+    @Override
+    public int step(int a, int b) {
+        // 执行具体的步骤操作
+        switch (this) {
+            case 收货:
+                return a + b;
+            case 检验:
+                return a * b;
+            case 打包:
+                return a - b;
+            case 发货:
+                return a / b;
+            case 完成:
+                return 0; // 或者其他适当的值
+            default:
+                throw new IllegalStateException("Unknown process step");
+        }
+    }
+
+    @Override
+    public Process previous() {
+        // 返回上一个步骤
+        return previousStep;
+    }
+
+    @Override
+    public abstract Process next(int a, int b);
+
+    private static Process previousStep = null;
+
+
+}
+
+interface ProcessItem {
+    int step(int a, int b);
+    Process previous();
+    Process next(int a, int b);
+}
---
+++ a01-spring-boot-learn/a01-02-java8/src/test/java/com/github/axinger/enumof/ProcessChainExample.java
@@ -0,0 +1,24 @@
+package com.github.axinger.enumof;
+
+public class ProcessChainExample {
+    public static void main(String[] args) {
+        Process currentStep = Process.收货;
+        int a = 5;
+        int b = 3;
+
+        currentStep = currentStep.next(a, b);
+
+        System.out.println("currentStep.previous() = " + currentStep.previous());
+
+        System.out.println("currentStep = " + currentStep);
+
+//        while (currentStep != null) {
+//            System.out.println("Current Step: " + currentStep);
+//            System.out.println("上一个: " + currentStep.previous());
+//            int result = currentStep.step(a, b);
+//            System.out.println("Result: " + result);
+//            ProcessItem.setCurrentStep(currentStep);
+//            currentStep = currentStep.next(a, b);
+//        }
+    }
+}
