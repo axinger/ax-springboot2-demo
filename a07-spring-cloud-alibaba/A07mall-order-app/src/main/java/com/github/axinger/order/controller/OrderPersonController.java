56f6a6e40dbfef223540423660e1f4b7bac9eeb8
axinger <liu_weixing@qq.com>
2025-03-27 22:14:29 +0800

    update

---
+++ a07-spring-cloud-alibaba/A07mall-order-app/src/main/java/com/github/axinger/order/controller/OrderPersonController.java
@@ -0,0 +1,24 @@
+package com.github.axinger.order.controller;
+
+import com.github.axinger.order.service.OrderPersonService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/person")
+public class OrderPersonController {
+
+    @Autowired
+    private OrderPersonService orderPersonService;
+
+    @GetMapping("/page")
+    public Object list() {
+        return orderPersonService.lambdaQuery()
+                .last("limit 1")
+                .list();
+    }
+}
