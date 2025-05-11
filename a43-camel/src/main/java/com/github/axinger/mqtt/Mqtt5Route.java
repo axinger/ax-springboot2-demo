8036244e94bbee1609cac6a21ebe4ab0a2b76819
axinger <liu_weixing@qq.com>
2025-05-12 00:30:13 +0800

    update

--- a43-camel/src/main/java/com/github/axinger/mqtt/Mqtt5Route.java
+++ a43-camel/src/main/java/com/github/axinger/mqtt/Mqtt5Route.java
@@ -1,6 +1,7 @@
 package com.github.axinger.mqtt;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +10,7 @@ public class Mqtt5Route extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("paho-mqtt5:testTopic?qos=2")
+        from("paho-mqtt5:testTopic?qos=1")
                 .log("MQTT连接建立成功，开始监听testTopic主题")
                 .log("Received MQTT message: ${body}")
                 .process(exchange -> {
@@ -17,17 +18,26 @@ public class Mqtt5Route extends RouteBuilder {
                     System.out.println("从MQTT订阅消息 Processing message: " + message);
                     // 打印消息头信息
                     System.out.println("消息头: " + exchange.getIn().getHeaders());
-
-//                    exchange.getContext().stop();
-
-
-                    // 发送处理确认
-//                    String ackTopic = "ack/" + exchange.getIn().getHeader("MqttMessageId");
-//                    producerTemplate.sendBody("paho-mqtt5:" + ackTopic, "ACK");
                 })
                 .onException(Exception.class)
                 .log("MQTT消息处理异常: ${exception.message}")
-//                .handled(true)
+                .handled(true)
                 .end();
+
+//        from("paho-mqtt5:testTopic2?qos=1")
+//                .log("MQTT连接建立成功，开始监听testTopic主题")
+//                .log("Received MQTT message: ${body}")
+//                // 添加幂等消费者
+////                .idempotentConsumer(header("messageId"), MemoryIdempotentRepository.memoryIdempotentRepository(200))
+//                .process(exchange -> {
+//                    String message = exchange.getIn().getBody(String.class);
+//                    System.out.println("从MQTT订阅消息 Processing message: " + message);
+//                    // 打印消息头信息
+//                    System.out.println("消息头: " + exchange.getIn().getHeaders());
+//                })
+//                .onException(Exception.class)
+//                .log("MQTT消息处理异常: ${exception.message}")
+//                .handled(true)
+//                .end();
     }
 }
--- a43-camel/src/main/java/com/github/axinger/mqtt/MqttTestController.java
+++ a43-camel/src/main/java/com/github/axinger/mqtt/MqttTestController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MqttTestController {
 
-    @Autowired
-    private ProducerTemplate producerTemplate;
-
     /*
 
     参数名	描述	示例
@@ -21,11 +18,15 @@ clientId	客户端ID	clientId=myClient1
 cleanStart	是否清除会话	cleanStart=true
 keepAliveInterval	保活间隔(秒)	keepAliveInterval=60
      */
+
+    @Autowired
+    private ProducerTemplate producerTemplate;
+
     @GetMapping("/send-mqtt")
     public String sendMqttMessage(String message) {
         try {
             // ?retained=true
-            producerTemplate.sendBody("paho-mqtt5:testTopic?qos=2", message);
+            producerTemplate.sendBody("paho-mqtt5:testTopic2?qos=1", message);
             return "Message sent to MQTT";
         } catch (Exception e) {
             return "发送失败: " + e.getMessage();
