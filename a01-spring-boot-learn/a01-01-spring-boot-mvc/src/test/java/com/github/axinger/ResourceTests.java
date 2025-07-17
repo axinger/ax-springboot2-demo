af65ba00d136c9fb4354adb9c49d15c7a826b7a8
axinger <liu_weixing@qq.com>
2025-07-17 06:47:58 +0000

    update
    Signed-off-by: axinger <liu_weixing@qq.com>

--- a01-spring-boot-learn/a01-01-spring-boot-mvc/src/test/java/com/github/axinger/ResourceTests.java
+++ a01-spring-boot-learn/a01-01-spring-boot-mvc/src/test/java/com/github/axinger/ResourceTests.java
@@ -1,38 +1,45 @@
 package com.github.axinger;
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.Resource;
 import cn.hutool.core.io.resource.ResourceUtil;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.util.ResourceUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 public class ResourceTests {
 
+    //org.springframework.util.ResourceUtils
     @Test
     public void test1() throws Exception {
-
         System.out.println("java当前目录,main函数,和springboot 项目不一样: " + System.getProperty("user.dir"));
 
-        File file = ResourceUtils.getFile("classpath:my.yml");
+        File file = ResourceUtils.getFile("classpath:123.json");
         String path1 = file.getAbsolutePath();
+
         System.out.println("path1 = " + path1);
 
+        String read = FileUtil.readString(file, StandardCharsets.UTF_8);
+        System.out.println("read = " + read);
     }
 
+    //org.springframework.core.io.ClassPathResource
     @Test
     public void test2() throws Exception {
 
 //        path2 = file:/D:/code/axinger/ax-springboot2-demo/a01-spring-boot-learn/a01-01-spring-boot-mvc/target/classes/my.yml
 //        path3 = D:\code\axinger\ax-springboot2-demo\a01-spring-boot-learn\a01-01-spring-boot-mvc\target\classes\my.yml
 //        ClassPathResource resource = new ClassPathResource("my.yml");
-        ClassPathResource resource = new ClassPathResource("mybatis-config.xml");
+        ClassPathResource resource = new ClassPathResource("123.json");
         String path2 = resource.getURL().toString();
         String filename = resource.getFilename();
         System.out.println("filename = " + filename);
@@ -41,63 +48,23 @@ public class ResourceTests {
         File file = resource.getFile();
         String path3 = file.getAbsolutePath();
         System.out.println("path3 = " + path3);
+        String read = FileUtil.readString(file, StandardCharsets.UTF_8);
+        System.out.println("read = " + read);
 
         InputStream inputStream = resource.getInputStream();
         System.out.println("inputStream = " + inputStream);
+        byte[] fileData = FileCopyUtils.copyToByteArray(inputStream);
+        String val = new String(fileData, StandardCharsets.UTF_8);
+        System.out.println("inputStream读取内容 = " + val);
     }
 
-    @Test
-    public void test3() throws InterruptedException, IOException {
-
-        System.out.println("java当前目录,main函数,和springboot 项目不一样: " + System.getProperty("user.dir"));
-
-        File file = ResourceUtils.getFile("classpath:test.js");
-        String path1 = file.getAbsolutePath();
-        System.out.println("path1 = " + path1);
-
-        ClassPathResource resource = new ClassPathResource("test.js");
-        File file2 = resource.getFile();
-        String path2 = file2.getAbsolutePath();
-
-        InputStream inputStream = resource.getInputStream();
-        System.out.println("inputStream = " + inputStream);
-
-        // 定义要执行的命令
-        String nodePath = "node"; // Node.js 的路径（如果已添加到环境变量，直接写 "node" 即可）
-        String jsFilePath = path2; // 替换为你的 JavaScript 文件路径
-
-        // 创建 ProcessBuilder 对象
-        ProcessBuilder processBuilder = new ProcessBuilder(nodePath, jsFilePath, "arg1", "arg2");
-
-        try {
-            // 启动进程
-            Process process = processBuilder.start();
-
-            // 读取输出流
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            // 等待进程结束并获取退出码
-            int exitCode = process.waitFor();
-            System.out.println("Exited with code: " + exitCode);
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        TimeUnit.SECONDS.sleep(5);
-    }
 
     //cn.hutool.core.io.resource
     @Test
-    public void test4(){
-        String string = ResourceUtil.readUtf8Str("123.json");
+    public void test4() {
+        String content = ResourceUtil.readUtf8Str("123.json");
 
-        System.out.println("string = " + string);
+        System.out.println("content = " + content);
 
         //当资源不存在时返回null
         InputStream inputStream = ResourceUtil.getStreamSafe("123.json");
