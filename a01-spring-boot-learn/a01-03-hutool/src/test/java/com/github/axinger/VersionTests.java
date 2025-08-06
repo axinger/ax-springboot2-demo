9bbb8e7562b413c19901fd6a2d496873ef12a584
axinger <liu_weixing@qq.com>
2025-08-06 21:47:26 +0800

    update

--- a01-spring-boot-learn/a01-03-hutool/src/test/java/com/github/axinger/VersionTests.java
+++ a01-spring-boot-learn/a01-03-hutool/src/test/java/com/github/axinger/VersionTests.java
@@ -15,25 +15,25 @@ public class VersionTests {
 
         // -1
         int compare = StrUtil.compareVersion("1.12.1", "1.12.1c");
-        System.out.println("1.12.1 >= 1.12.1c = " + (compare >=0));
+        System.out.println("1.12.1 >= 1.12.1c = " + (compare >= 0));
 
         int compare1 = StrUtil.compareVersion("1.0.2", "1.0.10");
-        System.out.println("1.0.2 >= 1.0.10 = " + (compare1 >=0));
+        System.out.println("1.0.2 >= 1.0.10 = " + (compare1 >= 0));
 
         // 1<2
         int compare2 = StrUtil.compareVersion("1", "2");
-        System.out.println("1 >= 2 = " + (compare2 >=0));
+        System.out.println("1 >= 2 = " + (compare2 >= 0));
 
         int compare3 = StrUtil.compareVersion("1.1.0", "1.11.0");
-        System.out.println("1.1.0 >= 1.11.0 = " + (compare3 >=0));
+        System.out.println("1.1.0 >= 1.11.0 = " + (compare3 >= 0));
 
         // 1=1 => 0
         int compare4 = StrUtil.compareVersion("1.0", "1");
-        System.out.println("1.0 > 1 = " +  (compare4>=0));
+        System.out.println("1.0 > 1 = " + (compare4 >= 0));
 
         // 2>1  => 1
         int compare5 = StrUtil.compareVersion("2", "1");
-        System.out.println("2 >1 = " + (compare5>=0));
+        System.out.println("2 >1 = " + (compare5 >= 0));
 
     }
 }
