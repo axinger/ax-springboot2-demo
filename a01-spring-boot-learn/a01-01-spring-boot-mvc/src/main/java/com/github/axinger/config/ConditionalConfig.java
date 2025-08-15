a659210da6b99162660e3bbcdd78bc98222899a3
axinger <liu_weixing@qq.com>
2025-08-16 02:22:26 +0800

    update

--- a01-spring-boot-learn/a01-01-spring-boot-mvc/src/main/java/com/github/axinger/config/ConditionalConfig.java
+++ a01-spring-boot-learn/a01-01-spring-boot-mvc/src/main/java/com/github/axinger/config/ConditionalConfig.java
@@ -2,6 +2,10 @@ package com.github.axinger.config;
 
 import com.github.axinger.model.bean.AppProperties;
 import com.github.axinger.model.bean.MyBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.cloud.CloudPlatform;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
@@ -9,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(AppProperties.class)
-public class AppConfig {
+public class ConditionalConfig {
 
 //    @ConditionalOnProperty(name = "my.feature.enabled", havingValue = "true")
 //    @ConditionalOnProperty(prefix = "my.app.cache", name = "preload", havingValue = "true")
@@ -25,6 +29,25 @@ public class AppConfig {
     @Bean("myBean2")
     @Conditional(MyCondition2.class)
     public MyBean myBean2(AppProperties appProperties) {
+        return new MyBean(appProperties);
+    }
+
+    @Bean("myBean3")
+//    @ConditionalOnExpression("${my.module.enabled:false} && ${my.module.mode} == 'advanced'")
+    @ConditionalOnExpression("#{environment['spring.profiles.active'] == 'dev' && systemProperties['user.country'] == 'CN'}")
+    public MyBean myBean3(AppProperties appProperties) {
+        return new MyBean(appProperties);
+    }
+
+    @Bean("myBean4")
+    @ConditionalOnProperty(name = "user.show", havingValue = "true")
+    public MyBean myBean4(AppProperties appProperties) {
+        return new MyBean(appProperties);
+    }
+
+    @Bean("myBean5")
+    @ConditionalOnCloudPlatform(CloudPlatform.SAP)
+    public MyBean myBean5(AppProperties appProperties) {
         return new MyBean(appProperties);
     }
 }
