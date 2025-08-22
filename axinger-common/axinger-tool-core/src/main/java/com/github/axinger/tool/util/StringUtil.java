5de9f880e66af1aae846ab7311c61401804c1fe7
axinger <liu_weixing@qq.com>
2025-08-22 21:17:32 +0800

    update

--- axinger-common/axinger-tool-core/src/main/java/com/github/axinger/tool/util/StringUtil.java
+++ axinger-common/axinger-tool-core/src/main/java/com/github/axinger/tool/util/StringUtil.java
@@ -1,5 +1,7 @@
 package com.github.axinger.tool.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -49,6 +51,22 @@ public class StringUtil {
         // 添加剩余未匹配部分
         matcher.appendTail(result);
         return result.toString();
+    }
+
+
+    /**
+     * 将时间日期字符串转换成cron表达式
+     *
+     * @param dateStr
+     * @return
+     */
+    public static String toCronExpression(String dateStr, Integer minute) {
+        DateTimeFormatter localDateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime date = LocalDateTime.parse(dateStr, localDateTimeFormat);
+        if (minute != null) {
+            date = date.plusMinutes(minute);
+        }
+        return date.getSecond() + " " + date.getMinute() + " " + date.getHour() + " " + date.getDayOfMonth() + " " + date.getMonthValue() + " " + "?";
     }
 
 }
