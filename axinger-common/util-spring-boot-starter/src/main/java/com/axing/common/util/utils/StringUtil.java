7f3e010dc396a03451b09c970ee8bb21f2f53c5c
axinger <liu_weixing@qq.com>
2025-08-22 07:55:41 +0000

    update
    Signed-off-by: axinger <liu_weixing@qq.com>

--- axinger-common/util-spring-boot-starter/src/main/java/com/axing/common/util/utils/StringUtil.java
+++ axinger-common/util-spring-boot-starter/src/main/java/com/axing/common/util/utils/StringUtil.java
@@ -10,6 +10,8 @@ public class StringUtil {
     /// 格式化字符串, ,未匹配的:姓名=jim,年龄=20,性别={sex},保留占位符
     public static String format(CharSequence template, Map<String, Object> paramsMap) {
         // 使用正则匹配所有 {key} 形式的占位符
+        // \\{ 和 \\}：匹配占位符的开头 { 和结尾 }（因为 { 和 } 在正则表达式中有特殊含义，所以需要用 \\ 转义）。
+        //(.+?)：这是一个捕获组，用来匹配 {} 中的任意内容（即 key），.+? 表示非贪婪匹配，即尽可能少地匹配字符。
         Pattern pattern = Pattern.compile("\\{(.+?)}");
         Matcher matcher = pattern.matcher(template);
         StringBuilder result = new StringBuilder();
