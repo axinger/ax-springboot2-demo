7743cd9bb4f425de82796217974f459d4e831da5
axinger <liu_weixing@qq.com>
2025-10-11 00:38:40 +0800

    update

--- a02-spring-boot-db/a02-13-duckdb/src/test/java/com/github/axinger/A02B13DuckDBApplicationTest.java
+++ a02-spring-boot-db/a02-13-duckdb/src/test/java/com/github/axinger/A02B13DuckDBApplicationTest.java
@@ -51,6 +51,7 @@ public class A02B13DuckDBApplicationTest {
         System.out.println("maps4 = " + maps4);
 
 
+        /// 不能修改文件,可以读取,再写入
         String sql5 = """
                 select t1.*,t2.*
                       from '/opt/123.json' t1
@@ -58,6 +59,69 @@ public class A02B13DuckDBApplicationTest {
                 """;
         List<Map<String, Object>> maps5 = jdbcTemplate.queryForList(sql5);
         System.out.println("maps5 = " + maps5);
+
+
+        String sql6 = """
+                    select id,
+                              name,
+                              age + 1 AS age,
+                              'Engineer' AS job  from '/opt/123.json'
+                """;
+        List<Map<String, Object>> maps6 = jdbcTemplate.queryForList(sql6);
+        System.out.println("maps6 = " + maps6);
+
+
+        /*
+            {"id":1,"name":"jim","age":11}
+            {"id":2,"name":"tom","age":21}
+         */
+        String sql7 = """
+                COPY (
+                  SELECT id, name, age + 1 AS age
+                  FROM '/opt/123.json'
+                ) TO '/opt/new_123_1.json' WITH (FORMAT JSON, ARRAY false);
+                """;
+        jdbcTemplate.execute(sql7);
+
+        /*
+            [
+            {"id":1,"name":"jim","age":11},
+            {"id":2,"name":"tom","age":21}
+            ]
+         */
+        String sql8 = """
+                COPY (
+                  SELECT id, name, age + 1 AS age
+                  FROM read_json('/opt/123.json')
+                ) TO '/opt/new_123_2.json' WITH (FORMAT JSON, ARRAY true);
+                """;
+        jdbcTemplate.execute(sql8);
+
+
+        /*
+        可以自定义分隔符：DELIMITER ';' 或 DELIMITER '|'
+         */
+        String sql9 = """
+              COPY (
+                SELECT id, name, age + 1 AS age
+                FROM '/opt/123.csv'
+              ) TO '/opt/new_123_1.csv' WITH (FORMAT CSV, HEADER true,DELIMITER '|');
+              """;
+        jdbcTemplate.execute(sql9);
+
+
+        /*
+        支持压缩：COMPRESSION GZIP（生成 .csv.gz） --- 变成16进制了
+         */
+        String sql10 = """
+              COPY (
+                SELECT id, name, age + 1 AS age
+                FROM '/opt/123.csv'
+              ) TO '/opt/new_123_10.csv' WITH (FORMAT CSV, HEADER true,COMPRESSION GZIP);
+              """;
+        jdbcTemplate.execute(sql10);
+
+
     }
 
 }
